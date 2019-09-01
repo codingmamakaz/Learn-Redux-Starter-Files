964f86154867244d08cd38c7d79685e92d1ced89
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-01 18:05:38 -0400

    Finish 14

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Photo from './photo'
+import Comments from './Comments'
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params
+    const i = this.props.posts.findIndex((post) => post.code === postId)
+    const post = this.props.posts[i]
+    const postComments = this.props.comments[postId] || []
     return (
       <div className="single-photo">
-        I'm single.
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     )
   }
--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,5 +1,5 @@
 import { createStore, compose } from 'redux'
-import { syncHistorywithStore } from 'react-router-redux'
+import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
 import rootReducer from './reducers/index'
@@ -14,5 +14,5 @@ const defaultState = {
 
 const store = createStore(rootReducer, defaultState)
 
-export const history = syncHistorywithStore(browserHistory, store)
+export const history = syncHistoryWithStore(browserHistory, store)
 export default store
