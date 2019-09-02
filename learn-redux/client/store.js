ff7a7094082cc1853a1c8c739df69f71246d89ce
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-02 14:23:47 -0400

    Redux dev tools

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,18 +1,28 @@
-import { createStore, compose } from 'redux'
-import { syncHistoryWithStore } from 'react-router-redux'
-import { browserHistory } from 'react-router'
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 
-import rootReducer from './reducers/index'
+// import the root reducer
+import rootReducer from './reducers/index';
 
-import comments from './data/comments'
-import posts from './data/posts'
+import comments from './data/comments';
+import posts from './data/posts';
 
+// create an object for the default data
 const defaultState = {
   posts,
   comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
 }
 
-const store = createStore(rootReducer, defaultState)
-
-export const history = syncHistoryWithStore(browserHistory, store)
-export default store
+export default store;
